d24388db119003937b183b966c9724c247bdaae0
Ahnaf Labib <yommslrb@gmail.com>
2024-10-23 18:33:26 -0400

    msg

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1 class="mainTitle">Welcome to Page Friend</h1>
+    <h1 class="mainTitle">Welcome </h1>
     <p class="paragraph">Discover and share books with others in the community.</p>
   </div>
 </template>
